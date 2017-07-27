2b8188913ff7c4dc465ac4afdba48eb07851d325
Nadia Okinina <nokinina@eurac.edu>
2017-07-27 16:56:01 +0200

    app/views/display/display_page.html.slim line 43: instead of xml_to_html(@page.xml_text) now there is @page.source_text. Because the XML processor used by FromThePage that transforms string to XML make empty tags auto-closed and therefore some tags don't show well in the Overview.

--- public/my-medium-onmousedown-functions.js
+++ public/my-medium-onmousedown-functions.js
@@ -5,46 +5,109 @@ transcriptionModule.init();
 setInterval(function () {
 transcriptionModule.repeatingFunction();
 }, 180000);
-$( ".button-greeting_id1" ).mousedown(function() {
+$( ".button-greeting_id2" ).mousedown(function() {
 var position = $(this).offset();
 var coords = {x:position.left, y:position.top};
 var categoryid=$(this).attr("data-categoryid");
-transcriptionModule.buttonFunction(categoryid,'greeting_id1',coords);
+transcriptionModule.buttonFunction(categoryid,'greeting_id2',coords);
 return false;
 });
-$( ".button-verb_id2" ).mousedown(function() {
+$( ".button-hey_id3" ).mousedown(function() {
 var position = $(this).offset();
 var coords = {x:position.left, y:position.top};
 var categoryid=$(this).attr("data-categoryid");
-transcriptionModule.buttonFunction(categoryid,'verb_id2',coords);
+transcriptionModule.buttonFunction(categoryid,'hey_id3',coords);
 return false;
 });
-$( ".button-predicate_id3" ).mousedown(function() {
+$( ".button-new_coll_first_subject_id4" ).mousedown(function() {
 var position = $(this).offset();
 var coords = {x:position.left, y:position.top};
 var categoryid=$(this).attr("data-categoryid");
-transcriptionModule.buttonFunction(categoryid,'predicate_id3',coords);
+transcriptionModule.buttonFunction(categoryid,'new_coll_first_subject_id4',coords);
 return false;
 });
-$( ".button-error_id5" ).mousedown(function() {
+$( ".button-second_new_coll__id5" ).mousedown(function() {
 var position = $(this).offset();
 var coords = {x:position.left, y:position.top};
 var categoryid=$(this).attr("data-categoryid");
-transcriptionModule.buttonFunction(categoryid,'error_id5',coords);
+transcriptionModule.buttonFunction(categoryid,'second_new_coll__id5',coords);
 return false;
 });
-$( ".button-test_free_input_id6" ).mousedown(function() {
+$( ".button-third_new_coll_id6" ).mousedown(function() {
 var position = $(this).offset();
 var coords = {x:position.left, y:position.top};
 var categoryid=$(this).attr("data-categoryid");
-transcriptionModule.buttonFunction(categoryid,'test_free_input_id6',coords);
+transcriptionModule.buttonFunction(categoryid,'third_new_coll_id6',coords);
 return false;
 });
-$( ".button-Duck_id7" ).mousedown(function() {
+$( ".button-second_coll_first_id7" ).mousedown(function() {
 var position = $(this).offset();
 var coords = {x:position.left, y:position.top};
 var categoryid=$(this).attr("data-categoryid");
-transcriptionModule.buttonFunction(categoryid,'Duck_id7',coords);
+transcriptionModule.buttonFunction(categoryid,'second_coll_first_id7',coords);
+return false;
+});
+$( ".button-second_coll_second_id8" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'second_coll_second_id8',coords);
+return false;
+});
+$( ".button-preposition_id9" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'preposition_id9',coords);
+return false;
+});
+$( ".button-bolt_id13" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'bolt_id13',coords);
+return false;
+});
+$( ".button-bullet_id11" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'bullet_id11',coords);
+return false;
+});
+$( ".button-link_id12" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'link_id12',coords);
+return false;
+});
+$( ".button-verb_id15" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'verb_id15',coords);
+return false;
+});
+$( ".button-paragraph_id16" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'paragraph_id16',coords);
+return false;
+});
+$( ".button-greeting_id18" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'greeting_id18',coords);
+return false;
+});
+$( ".button-error_id21" ).mousedown(function() {
+var position = $(this).offset();
+var coords = {x:position.left, y:position.top};
+var categoryid=$(this).attr("data-categoryid");
+transcriptionModule.buttonFunction(categoryid,'error_id21',coords);
 return false;
 });
 });
