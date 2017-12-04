7013d6e9aad524ad195b0b4c76bb1576b8c9702f
Nadia Okinina <nokinina@eurac.edu>
2017-12-04 13:01:40 +0100

    Deleted unnecessary files from the public folder

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -654,7 +654,7 @@ class CategoryController < ApplicationController
     mediumOnmouseoverFunctions+="});"
 
     #File.write(ActionController::Base.helpers.asset_path('medium-tag-styles.css'), styleInstructions)
-    File.write('app/assets/javascripts/medium-tag-styles.css', styleInstructions)
+    File.write('app/assets/stylesheets/medium-tag-styles.css', styleInstructions)
     #File.write('public/medium-tag-styles.css', styleInstructions)
 
     #File.write(ActionController::Base.helpers.asset_path('my-medium-onmousedown-functions.js'), mediumOnmouseoverFunctions)
@@ -666,7 +666,7 @@ class CategoryController < ApplicationController
   def discard_all_styles
     Categorystyle.delete_all()
     #File.write(ActionController::Base.helpers.asset_path('medium-tag-styles.css'), styleInstructions)
-    File.write('app/assets/javascripts/medium-tag-styles.css', styleInstructions)
+    File.write('app/assets/stylesheets/medium-tag-styles.css', styleInstructions)
     #File.write('public/medium-tag-styles.css', '')
     flash[:notice] = "Category styles have been discarded."
     anchor = "#category-#{@category.id}"
